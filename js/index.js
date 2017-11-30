469a7de67e95ca5e58002f6d5fb15af99b19eed6
unknown <denghaibo@dianshitech.sh>
2017-11-30 16:55:44 +0800

    update p3

--- js/index.js
+++ js/index.js
@@ -37,6 +37,38 @@ $(function() {
 		$('.part_3').fadeIn(500);
 		$('.part_3').addClass('animate');
 		$($('.part_3 .head_bg')[t]).removeClass('hide');
+		addKeyTouchFun();
 	}
+
+	function addKeyTouchFun(){
+		var key = document.getElementById("key");
+		var max_length = 4;
+		var dis_touch = '';
+		key.addEventListener('touchstart', function(event){
+			console.log('开始拖动', event);
+		});
+
+		key.addEventListener('touchmove', function(event){
+			event.preventDefault();
+			console.log('滑动继续');
+			dis_touch = event.targetTouches[0].pageX / 100;
+			if(dis_touch > max_length) {
+				dis_touch = max_length;
+			}
+			$(key).css('transition', 'none').css('transform', 'translateX(' + dis_touch + 'rem');
+		});
+
+		key.addEventListener('touchend', function(event){
+			console.log('停止滑动', event);
+			var dis_touch = event.changedTouches[0].pageX / 100;
+			if(dis_touch < max_length) {
+				dis_touch = 0;
+				$(key).css('transition', 'all 0.5s').css('transform', 'translateX(' + dis_touch + 'rem');
+			}else {
+				//过度页
+			}
+		});
+	}
+
 
 });
