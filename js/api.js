edb40ec9b85c8582b8796c0e0fd74a6561d201c3
Real <369517224@qq.com>
2017-12-02 02:30:00 +0800

    修改目录结构

--- js/api.js
+++ js/api.js
@@ -1,4 +1,17 @@
 //获取code然后登录获取用户信息接口
 function login(code,successCB){
-
+  var timestamp = Math.floor(new Date.getTime()/1000);
+  var sign = md5(code+timestamp+Config.SIGN_KEY);
+  $.ajax({
+    url : Config.API_URL+'login.php',
+    type : 'GET',
+    data : {
+      code      : code,
+      timestamp : timestamp,
+      sign      : sign
+    },
+    success : function(data){
+      successCB();
+    }
+  })
 }
--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var Config = {
   MAX_PIC_SIZE : 5242880,
   API_URL      : 'http://localhost/git/www.adleading.com/api/huazhu/',            //测试
-  API_URL      : 'http://www.adleading.com/api/huazhu/',            //测试
-  SIGN         : '127huazhugdmh8z',
+  // API_URL      : 'http://www.adleading.com/api/huazhu/',            //正式
+  SIGN_KEY     : '127huazhugdmh8z',
 }
