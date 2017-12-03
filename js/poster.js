ce1d39b1af18e4e8da51ce9a0a6ea8c350d22f2d
Real <369517224@qq.com>
2017-12-03 21:37:28 +0800

    添加接口

--- js/poster.js
+++ js/poster.js
@@ -1,22 +1,40 @@
-$('#file').change(function() {
-  var file = this.files[0];
-  console.log(file);
-  if(file.size>=Config.MAX_PIC_SIZE){
-    alert('图像大小超过限制');
-    return;
-  }else if(file.type.indexOf('gif')>-1){
-    alert('无法上传GIF图片');
-    return;
+var pc = new PhotoClip('.view', {
+  size: [document.documentElement.clientWidth,document.documentElement.clientHeight],
+  outputSize: document.documentElement.clientWidth,
+  //adaptive: ['60%', '80%'],
+  file: '#file',
+  // view: '#view',
+  ok: '.clipBtn',
+  //img: 'img/mm.jpg',
+  loadStart: function() {
+    console.log('开始读取照片');
+    $('.poster').hide();
+    $('.clip').fadeIn(800);
+    $('.html2canvas').show();
+  },
+  loadComplete: function() {
+    console.log('照片读取完成');
+  },
+  done: function(dataUrl) {
+    $('.poster .bg').attr('src',dataUrl);
+    $('.clip').fadeOut();
+    $('.poster').fadeIn(800);
+    setTimeout(function(){
+      html2canvas(document.querySelector('.html2canvas'), {
+        onrendered: function(canvas) {
+          $('.html2canvas').hide();
+          // $('.layer').after(canvas);
+          $('.final').attr('src',canvas.toDataURL());
+        },
+        // logging : true,
+      });
+    },1000)
+
+  },
+  fail: function(msg) {
+    alert(msg);
   }
-  var reader = new FileReader();
-  var canvas = document.getElementById("canvas").getContext("2d");
-  reader.readAsDataURL(file);
-  reader.addEventListener('load', function() {
-    var img = document.createElement('img');
-    img.src = reader.result;
-    // img.width=remToPx(7.5);
-    // img.height=remToPx(7.5);
-    // console.log(img.src)
-    canvas.drawImage(img,0,0,375,375)
-  });
-})
+});
+function changePic(){
+  $('#file').click();
+}
