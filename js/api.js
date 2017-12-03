20592cd5c87cddd5f4d33a11b32ebb1ba89cb0f1
Bob Deng <ly5569dhb@sina.cn>
2017-12-03 22:23:06 +0800

    update z-index

--- js/api.js
+++ js/api.js
@@ -1,6 +1,31 @@
+function $_GET(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) return unescape(r[2]);
+  return null;
+}
+var validate = {
+  isEmpty: function(val) {
+    if (val == "") {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  isMobile: function(val) {
+    if (val == "") {
+      return false;
+    }
+    if (!val.match(/^1[3|4|5|7|8][0-9]\d{4,8}$/) || val.length != 11) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+}
 //获取code然后登录获取用户信息接口
 function login(code,successCB){
-  var timestamp = Math.floor(new Date.getTime()/1000);
+  var timestamp = Math.floor(new Date().getTime()/1000);
   var sign = md5(code+timestamp+Config.SIGN_KEY);
   $.ajax({
     url : Config.API_URL+'login.php',
@@ -11,7 +36,83 @@ function login(code,successCB){
       sign      : sign
     },
     success : function(data){
-      successCB();
+      localStorage.id = data.id
+      successCB(data);
+    }
+  })
+}
+//完善用户信息
+//id是login接口返回的id
+//image:base64的
+//sobg_list 数组？待定
+function complete_user(id,image,name,record_name,song_list,mobile,successCB){
+  var timestamp = Math.floor(new Date().getTime()/1000);
+  var sign = md5(id+timestamp+Config.SIGN_KEY);
+  if(!validate.isEmpty(name)){
+    alert('请填写姓名');
+    return;
+  }else{
+    for (var i = 0; i < MaskWord.text.length; i ++) {
+      if(name.indexOf(MaskWord.text[i]) > -1) {
+        console.log('有屏蔽字，屏蔽字为：',MaskWord.text[i]);
+        alert('含有屏蔽字')
+        break;
+        return;
+      }
+    }
+  }
+  if(!validate.isEmpty(record_name)){
+    alert('请填写唱片名');
+    return;
+  }else{
+    for (var i = 0; i < MaskWord.text.length; i ++) {
+      if(record_name.indexOf(MaskWord.text[i]) > -1) {
+        console.log('有屏蔽字，屏蔽字为：',MaskWord.text[i]);
+        alert('含有屏蔽字');
+        break;
+        return;
+      }
+    }
+  }
+  if(!validate.isMobile(mobile)){
+    alert('请填写正确的手机');
+    return;
+  }
+  $.ajax({
+    url : Config.API_URL+'complete_user.php',
+    type : 'POST',
+    data : {
+      id           : id,
+      image        : image,
+      name         : name,
+      recorde_name : recorde_name,
+      song_list    : song_list,
+      mobile       : mobile,
+      timestamp    : timestamp,
+      sign         : sign,
+    },
+    success : function(data){
+      console.log('完善用户信息返回',data);
+      successCB(data);
+    }
+  })
+}
+//点赞操作，
+function follow(id,follow_id,successCB){
+  var timestamp = Math.floor(new Date().getTime()/1000);
+  var sign = md5(id+follow_id+timestamp+Config.SIGN_KEY);
+  $.ajax({
+    url : Config.API_URL+'follow.php',
+    type : 'GET',
+    data : {
+      id           : id,
+      follow_id    : follow_id,
+      timestamp    : timestamp,
+      sign         : sign,
+    },
+    success : function(data){
+      console.log('完善用户信息返回',data);
+      successCB(data);
     }
   })
 }
--- js/index.js
+++ js/index.js
@@ -1,116 +1,122 @@
 $(function() {
-	var p1_btn = document.getElementById("p1_btn");
-	var dis_tim = 1000;
-	var timeout = undefined;
-	createParallax();
-	//长按屏幕
-	var sTime = 0;
-	var eTime = 0;
-	var flag = false;
-	p1_btn.addEventListener('touchstart', function(event) {
-		sTime = new Date().getTime();
-		timeout = setTimeout(function(){
-			flag = true;
-			togglePage2();
-		}, dis_tim);
-		event.preventDefault();
-	}, false);
+  var pic_base64 = '';
+  var p1_btn = document.getElementById("p1_btn");
+  var dis_tim = 1000;
+  var timeout = undefined;
+  createParallax();
+  //长按屏幕
+  var sTime = 0;
+  var eTime = 0;
+  var flag = false;
+  p1_btn.addEventListener('touchstart', function(event) {
+    sTime = new Date().getTime();
+    timeout = setTimeout(function(){
+      flag = true;
+      togglePage2();
+    }, dis_tim);
+    event.preventDefault();
+  }, false);
 
-	p1_btn.addEventListener('touchmove', function(event) {
-		event.preventDefault();
-	});
+  p1_btn.addEventListener('touchmove', function(event) {
+    event.preventDefault();
+  });
 
-	p1_btn.addEventListener('touchend', function(event) {
-		event.preventDefault();
-		eTime = new Date().getTime();
+  p1_btn.addEventListener('touchend', function(event) {
+    event.preventDefault();
+    eTime = new Date().getTime();
 
-		if(eTime - sTime >= dis_tim && !flag){
-			togglePage2()
-		}else {
-			console.log('长按');
-			clearTimeout(timeout);
-		}
+    if(eTime - sTime >= dis_tim && !flag){
+      togglePage2()
+    }else {
+      console.log('长按');
+      clearTimeout(timeout);
+    }
 
-	});
+  });
 
-	function togglePage2(){
-		$('.part_1').removeClass('animate');
-		$('.part_1').fadeOut(500);
-		$('.part_2').fadeIn(500);
+  function togglePage2(){
+    $('.part_1').removeClass('animate');
+    $('.part_1').fadeOut(500);
+    $('.part_2').fadeIn(500);
     $('.part_2').addClass('animate');
-	}
+  }
 
-	setTimeout(function() {
-		$(".part_1").addClass("animate");
-	}, 500);
+  setTimeout(function() {
+    $(".part_1").addClass("animate");
+  }, 500);
 
-	var p2_note = document.querySelector('.part_2').querySelectorAll('.note');
-	for(var i = 0; i < 3; i++){
-		(function(t){
-			var item = p2_note[t];
-			item.onclick = function(){
-				$(item).prev().css('display', 'block');
-				togglePage3(t);
-			};
-		})(i);
-	}
+  var p2_note = document.querySelector('.part_2').querySelectorAll('.note');
+  for(var i = 0; i < 3; i++){
+    (function(t){
+      var item = p2_note[t];
+      item.onclick = function(){
+        $(item).prev().css('display', 'block');
+        togglePage3(t);
+      };
+    })(i);
+  }
 
-	function togglePage3(t){
-		$('.part_2').removeClass('animate');
-		$('.part_2').fadeOut(500);
-		$('.part_3').fadeIn(500);
-		$('.part_3').addClass('animate');
-		$($('.part_3 .head_bg')[t]).removeClass('hide');
-		addKeyTouchFun();
-	}
-
-	function createParallax(){
-		var scene_1 = $('.layer_1').get(0);
-	  var parallax = new Parallax(scene_1, {
-	    pointerEvents: true,
-	  });
-	  var scene_2 = $('.layer_2').get(0);
-	  var parallax = new Parallax(scene_2, {
-	    pointerEvents: true,
-	  });
-	  var scene_3 = $('.layer_3').get(0);
-	  var parallax = new Parallax(scene_3, {
-	    pointerEvents: true,
-	  });
-	}
-
-	function addKeyTouchFun(){
-		var key = document.getElementById("key");
-		var max_length = 4;
-		var dis_touch = '';
-		var st_pos = 0;
-		key.addEventListener('touchstart', function(event){
-			console.log('开始拖动', event);
-			st_pos = parseInt(event.targetTouches[0].pageX);
-		});
-
-		key.addEventListener('touchmove', function(event){
-			event.preventDefault();
-			console.log('滑动继续');
-			dis_touch = pxToRem(event.targetTouches[0].pageX - st_pos);
-
-			if(dis_touch > max_length) {
-				dis_touch = max_length;
-			}
-			$(key).css('transition', 'none').css('transform', 'translateX(' + dis_touch + 'rem');
-		});
-
-		key.addEventListener('touchend', function(event){
-			console.log('停止滑动', event);
-			var dis_touch = event.changedTouches[0].pageX / 100;
-			if(dis_touch < max_length) {
-				dis_touch = 0;
-				$(key).css('transition', 'all 0.5s').css('transform', 'translateX(' + dis_touch + 'rem');
-			}else {
-				//过度页
-			}
-		});
-	}
+  function togglePage3(t){
+    $('.part_2').removeClass('animate');
+    $('.part_2').fadeOut(500);
+    $('.part_3').fadeIn(500);
+    $('.part_3').addClass('animate');
+    $($('.part_3 .head_bg')[t]).removeClass('hide');
+    addKeyTouchFun();
+  }
 
 
+  function createParallax(){
+    var scene_1 = $('.layer_1').get(0);
+    var parallax = new Parallax(scene_1, {
+      pointerEvents: true,
+    });
+    var scene_2 = $('.layer_2').get(0);
+    var parallax = new Parallax(scene_2, {
+      pointerEvents: true,
+    });
+    var scene_3 = $('.layer_3').get(0);
+    var parallax = new Parallax(scene_3, {
+      pointerEvents: true,
+    });
+  }
+
+  function addKeyTouchFun(){
+    var key = document.getElementById("key");
+    var max_length = 4;
+    var dis_touch = '';
+    var st_pos = 0;
+    key.addEventListener('touchstart', function(event){
+      console.log('开始拖动', event);
+      st_pos = parseInt(event.targetTouches[0].pageX);
+    });
+
+    key.addEventListener('touchmove', function(event){
+      event.preventDefault();
+      console.log('滑动继续');
+      dis_touch = pxToRem(event.targetTouches[0].pageX - st_pos);
+
+      if(dis_touch > max_length) {
+        dis_touch = max_length;
+      }
+      $(key).css('transition', 'none').css('transform', 'translateX(' + dis_touch + 'rem');
+    });
+
+    key.addEventListener('touchend', function(event){
+      console.log('停止滑动', event);
+      var dis_touch = event.changedTouches[0].pageX / 100;
+      if(dis_touch < max_length) {
+        dis_touch = 0;
+        $(key).css('transition', 'all 0.5s').css('transform', 'translateX(' + dis_touch + 'rem');
+      }else {
+        //过度页
+      }
+    });
+  }
+  //登录
+  var code = $_GET('code');
+  console.log('code:',code);
+  login(code,function(data){
+    console.log(data)
+  })
 });
