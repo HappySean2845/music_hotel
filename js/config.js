a0064e42126cce8ccecfd8496b0bda0d26d67cbf
Real <369517224@qq.com>
2017-12-05 01:33:33 +0800

    继续完善

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var Config = {
   MAX_PIC_SIZE : 5242880,
-  API_URL      : 'http://localhost/git/www.adleading.com/api/huazhu/',            //测试
-  // API_URL      : 'http://www.adleading.com/api/huazhu/',            //正式
+  // API_URL      : 'http://localhost/git/www.adleading.com/api/huazhu/',            //测试
+  API_URL      : 'http://www.adleading.com/api/huazhu/',            //正式
   SIGN_KEY     : '127huazhugdmh8z',
   LOGIN_URL    : 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5b621c2653ac8bc1&redirect_uri=http%3a%2f%2fharman.adleading.com%2fhome%2findex%2fredirect%3fredirect%3dhttp%3a%2f%2flocalhost%2fgit%2fmusic_hotel%2findex.html&response_type=code&scope=snsapi_base&state=0#wechat_redirect'
 }
