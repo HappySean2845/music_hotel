f5b7f61bdd74073fb37d2a6de83fed5e15247c95
Bob Deng <ly5569dhb@sina.cn>
2017-12-06 01:06:28 +0800

    touch

--- js/index.js
+++ js/index.js
@@ -39,10 +39,9 @@ $(function() {
   }
 
   setTimeout(function() {
-    $(".part_1").addClass("animate");
+    // $(".part_1").addClass("animate");
+    toggleShakePage1();
   }, 500);
-
-
 
   var p2_note = document.querySelector('.part_2').querySelectorAll('.note');
   for(var i = 0; i < 3; i++){
@@ -82,7 +81,7 @@ $(function() {
 
   function addKeyTouchFun(){
     var key = document.getElementById("key");
-    var max_length = 4;
+    var max_length = 3.8;
     var dis_touch = '';
     var st_pos = 0;
     key.addEventListener('touchstart', function(event){
@@ -103,64 +102,64 @@ $(function() {
 
     key.addEventListener('touchend', function(event){
       console.log('停止滑动', event);
-      var dis_touch = event.changedTouches[0].pageX / 100;
-      if(dis_touch < max_length-10) {
-        dis_touch = 0;
-        $(key).css('transition', 'all 0.5s').css('transform', 'translateX(' + dis_touch + 'rem');
-      }else {
+      var dis_touch = pxToRem(event.changedTouches[0].pageX - st_pos);
+      console.log(dis_touch);
+      if(dis_touch >= max_length) {
         //上传照片
-          changePic();
+        changePic();
       }
+      dis_touch = 0;
+      $(key).css('transition', 'all 0.5s').css('transform', 'translateX(' + dis_touch + 'rem');
     });
   }
-  var iframe = getXiamiPlayer();
-  $('.btn_group').after(iframe);
-  //登录
-  var code = $_GET('code');
-  console.log('code:',code);
-  if(!localStorage.id||localStorage.id=='undefined'){
-    login(code,function(data){
-      console.log(data)
-    })
-  }
-  $('.submit').click(function(){
-    uploadImage(localStorage.id,img_base64,function(data){
-      console.log("进入留资页面");
-      $(".poster").fadeOut(500);
-      $("#infoContainer").fadeIn(500);
-    })
-  })
-  $("#infoContainer .btn_confirm").click(function(){
-    complete_user(localStorage.id,window.img_base64,$("#name").val(),$("#record_name").val(),window.user_given_music_list,$("#mobile").val(),function(){
-      alert("留资成功");   //跳转到排行榜界面
-      $("#infoContainer").fadeOut(500);
-      $("#topContainer").fadeIn(500);
-      getRank(localStorage.id,function(o){
-        // if(o&&o.length>=1){
-        console.log('list',o);
-          var inner = "";
-        var d = JSON.parse(o)["list"];
-        if(!d) return;
-        for(var i = 0;i<d.length;i++){
-            var eachLine = '<li>' +
-              '<span class="icon"><img src="img/darre/darre_music_name_19.png" alt=""></span>' +
-              '<span class="record_name">'+d[i].record_name+' </span>' +
-              '<span class="star"><img src="img/darre/darre_music_star_22.png" alt="">'+d[i].follow+' </span>' +
-              '<span class="play"><img src="img/darre/darre_music_play_19.png" alt=""></span></li>';
-            inner = inner + eachLine;
-          }
-        $(".top_list ul").html(inner);
-      });
-    })
-  })
-  $("#topContainer .how").click(function(){
-    console.log("出玩法提示");
-    $("#topContainer .help").fadeIn(500);
-  })
-  $("#topContainer .help").click(function(){
-    console.log("移除玩法提示");
-    $("#topContainer .help").fadeOut(500);
-  })
+  // var iframe = getXiamiPlayer();
+  // $('.btn_group').after(iframe);
+  // //登录
+  // var code = $_GET('code');
+  // console.log('code:',code);
+  // if(!localStorage.id||localStorage.id=='undefined'){
+  //   login(code,function(data){
+  //     console.log(data)
+  //   })
+  // }
+  // $('.submit').click(function(){
+  //   uploadImage(localStorage.id,img_base64,function(data){
+  //     console.log("进入留资页面");
+  //     $(".poster").fadeOut(500);
+  //     $("#infoContainer").fadeIn(500);
+  //   })
+  // })
+  // $("#infoContainer .btn_confirm").click(function(){
+  //   complete_user(localStorage.id,window.img_base64,$("#name").val(),$("#record_name").val(),window.user_given_music_list,$("#mobile").val(),function(){
+  //     alert("留资成功");   //跳转到排行榜界面
+  //     $("#infoContainer").fadeOut(500);
+  //     $("#topContainer").fadeIn(500);
+  //     getRank(localStorage.id,function(o){
+  //       // if(o&&o.length>=1){
+  //       console.log('list',o);
+  //         var inner = "";
+  //       var d = JSON.parse(o)["list"];
+  //       if(!d) return;
+  //       for(var i = 0;i<d.length;i++){
+  //           var eachLine = '<li>' +
+  //             '<span class="icon"><img src="img/darre/darre_music_name_19.png" alt=""></span>' +
+  //             '<span class="record_name">'+d[i].record_name+' </span>' +
+  //             '<span class="star"><img src="img/darre/darre_music_star_22.png" alt="">'+d[i].follow+' </span>' +
+  //             '<span class="play"><img src="img/darre/darre_music_play_19.png" alt=""></span></li>';
+  //           inner = inner + eachLine;
+  //         }
+  //       $(".top_list ul").html(inner);
+  //     });
+  //   })
+  // })
+  // $("#topContainer .how").click(function(){
+  //   console.log("出玩法提示");
+  //   $("#topContainer .help").fadeIn(500);
+  // })
+  // $("#topContainer .help").click(function(){
+  //   console.log("移除玩法提示");
+  //   $("#topContainer .help").fadeOut(500);
+  // })
 
   // 切摇一摇页面 
   // 添加此方法即可
