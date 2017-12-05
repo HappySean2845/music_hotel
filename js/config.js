0b296f1e7a03b7ce0af7d44f150ead84abec8097
Real <369517224@qq.com>
2017-12-06 01:19:48 +0800

    asdasd

--- js/config.js
+++ js/config.js
@@ -6,6 +6,6 @@ var Config = {
   LOGIN_URL    : 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5b621c2653ac8bc1&redirect_uri=http%3a%2f%2fharman.adleading.com%2fhome%2findex%2fredirect%3fredirect%3dhttp%3a%2f%2flocalhost%2fgit%2fmusic_hotel%2findex.html&response_type=code&scope=snsapi_base&state=0#wechat_redirect',
   // HOTEL_URL    : 'http://www.adleading.com/music_hotel/index.html?frompage=hotel',
   // WEB_URL      : 'http://www.adleading.com/music_hotel/index.html?frompage=web',
-  HOTEL_URL    : 'http://localhost/git/music_hotel/index.html?frompage=hotel',
-  WEB_URL      : 'http://localhost/git/music_hotel/index.html?frompage=web',
+  HOTEL_URL    : 'http://192.168.1.5/git/music_hotel/index.html?frompage=hotel',
+  WEB_URL      : 'http://192.168.1.5/git/music_hotel/index.html?frompage=web',
 }
